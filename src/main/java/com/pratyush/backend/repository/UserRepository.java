5ec04c0339e9b3f63474c07579f1f1a89199021c
Pratyuxxh <107082402+Pratyuxxh@users.noreply.github.com>
2022-09-30 17:14:29 +0530

    Initial commit

---
+++ src/main/java/com/pratyush/backend/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.pratyush.backend.repository;
+
+import com.pratyush.backend.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
---
+++ src/main/java/com/pratyush/backend/service/UserService.java
@@ -0,0 +1,5 @@
+package com.pratyush.backend.service;
+
+public interface UserService {
+
+}
