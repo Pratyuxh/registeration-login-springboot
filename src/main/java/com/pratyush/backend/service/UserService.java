5cbe53017d6c1bb864502e740867b3060cef8a78
Pratyuxxh <107082402+Pratyuxxh@users.noreply.github.com>
2022-10-01 11:56:32 +0530

    services-security-updated

--- src/main/java/com/pratyush/backend/service/UserService.java
+++ src/main/java/com/pratyush/backend/service/UserService.java
@@ -1,9 +1,11 @@
 package com.pratyush.backend.service;
 
+
+import org.springframework.security.core.userdetails.UserDetailsService;
+
 import com.pratyush.backend.model.User;
-import com.pratyush.backend.web.DTO.UserRegistration;
+import com.pratyush.backend.web.DTO.UserRegistrationDto
 
-public interface UserService {
-    User save(UserRegistration registeration);
-
+public interface UserService extends UserDetailsService{
+    User save(UserRegistrationDto registrationDto);
 }
--- src/main/java/com/pratyush/backend/service/UserServiceImpl.java
+++ src/main/java/com/pratyush/backend/service/UserServiceImpl.java
@@ -3,25 +3,54 @@ package com.pratyush.backend.service;
 import com.pratyush.backend.model.Role;
 import com.pratyush.backend.model.User;
 import com.pratyush.backend.repository.UserRepository;
-import com.pratyush.backend.web.DTO.UserRegistration;
-import org.springframework.stereotype.Service;
+import com.pratyush.backend.web.DTO.UserRegistrationDto;
 
 import java.util.Arrays;
+import java.util.Collection;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
 
 @Service
 public class UserServiceImpl implements UserService{
 
     private UserRepository userRepository;
 
+    @Autowired
+    private BCryptPasswordEncoder passwordEncoder;
+
     public UserServiceImpl(UserRepository userRepository) {
+        super();
         this.userRepository = userRepository;
     }
 
     @Override
-    public User save(UserRegistration registeration) {
-        User user  = new User(registeration.getFirstName(), registeration.getLastName(),
-                                registeration.getEmail(), registeration.getPassword(),
-                Arrays.asList(new Role("ROLE_USER")));
+    public User save(UserRegistrationDto registrationDto) {
+        User user = new User(registrationDto.getFirstName(),
+                registrationDto.getLastName(), registrationDto.getEmail(),
+                passwordEncoder.encode(registrationDto.getPassword()), Arrays.asList(new Role("ROLE_USER")));
+
         return userRepository.save(user);
     }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        User user = userRepository.findByEmail(username);
+        if(user == null) {
+            throw new UsernameNotFoundException("Invalid username or password.");
+        }
+        return new org.springframework.security.core.userdetails.User(user.getEmail(), user.getPassword(), mapRolesToAuthorities(user.getRoles()));
+    }
+
+    private Collection<? extends GrantedAuthority> mapRolesToAuthorities(Collection<Role> roles){
+        return roles.stream().map(role -> new SimpleGrantedAuthority(role.getName())).collect(Collectors.toList());
+    }
+
 }
